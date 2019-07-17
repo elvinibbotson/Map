691db205a7075c540447b5370372be1e7dd862f9
elvinibbotson <elvinibbotson@outlook.com>
2019-07-17 14:53:19 +0100

    Add files via upload

--- peakScript.js
+++ peakScript.js
@@ -130,9 +130,8 @@
 	}
 	centreMap(); // go to saved location
 	metric=window.localStorage.getItem("metric");
-	console.log("metric is "+metric);
 	id('metric').checked=metric;
-	console.log('metric.checked is '+id('metric').checked)
+	console.log("metric is "+metric+' - checked is '+id('metric').checked)
 	// get list of saved tracks
 	var json=JSON.parse(window.localStorage.getItem("peakTracks"));
 	console.log("routes: "+json);
