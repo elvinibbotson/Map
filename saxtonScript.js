9e26bb454f7a64ef5a05ee8c1c918ff125a36e58
elvinibbotson <elvinibbotson@outlook.com>
2024-09-26 12:35:02 +0100

    Add files via upload

--- saxtonScript.js
+++ saxtonScript.js
@@ -1,10 +1,8 @@
 	"use strict";
 	var map; // CyclOSM map
-	var profilesCanvas; // canvas for track altitude profiles
-	var x, y, x0, y0; // horizontal and vertical coordinates/measurements
+	var x,y; // horizontal and vertical coordinates/measurements
 	var json;
 	var routing=false;
-	var enroute=false;
 	var ready=false;
 	var tracking=false;
 	var trackNames=[];
@@ -23,7 +21,6 @@
 	var compass="N  NNENE ENEE  ESESE SSES  SSWSW WSWW  WNWNW NNWN  ";
 	var months="JanFebMarAprMayJunJulAugSepOctNovDec";
 	var notifications=[];
-	
 	// EVENT HANDLERS
 	id('plusButton').addEventListener('click',function(){
 		map.zoomIn();
@@ -39,7 +36,7 @@
 	});
 	id("actionButton").addEventListener("click",getFix);
 	id("stopButton").addEventListener("click",cease);
-	id("saveButton").addEventListener("click",saver);
+	id("saveButton").addEventListener("click",saveRoute);
 	id('moreButton').addEventListener('click',function(){
 		show('moreButton',false);
 		id('more').style.display='block';
@@ -85,7 +82,9 @@
 	    show('actionButton',true);
 	    // redraw();
 	});
+	id('routeName').addEventListener('change',renameRoute);
 	id('loadButton').addEventListener('click',loadRoute);
+	id('deleteButton').addEventListener('click',deleteRoute);
 	// ADD EVENTS FOR renameButton, refineButton and removeButton
 	id('unitButton').addEventListener('click',function(){
 		if(unit=='km') unit='mi';
@@ -99,15 +98,11 @@
 	  routing=false;
 	  nodes=[];
 	});
-	
 	var sw=window.innerWidth;
 	var sh=window.innerHeight;
 	console.log("screen size: "+sw+"x"+sh);
 	id('map').style.width=sw+'px';
 	id('map').style.height=sh+'px';
-	profilesCanvas=id("profilesCanvas").getContext("2d"); // set up drawing canvas
-	id("profilesCanvas").width=sw;
-	id("profilesCanvas").height=sh*0.2;
 	id('locus').style.left=(sw/2-12)+'px';
 	id('locus').style.top=(sh/2-12)+'px';
 	show('map',true);
@@ -135,6 +130,11 @@
 	console.log('saved zoom: '+zoom);
 	if(zoom===null) zoom=10;
 	unit=window.localStorage.getItem('unit');
+	json=JSON.parse(window.localStorage.getItem("saxtonRoutes"));
+	if(json!==null) {
+		routeNames=json.names;
+		notify(routeNames.length+' routes');
+	}
 	console.log('unit: '+unit);
 	id('unitButton').innerText=unit;
 	map=L.map('map',{zoomControl: false}).setView([lat,lng],zoom); // default location in Derbyshire
@@ -212,19 +212,6 @@
 	function onLocationError(e) {
 		alert(e.message);
 	}
-	// get list of saved tracks
-	var json=JSON.parse(window.localStorage.getItem("saxtonTracks"));
-	console.log("routes: "+json);
-	if(json!=null) {
-		trackNames=json.names;
-		notify(trackNames.length+' tracks');
-	}
-	json=JSON.parse(window.localStorage.getItem("saxtonRoutes"));
-	if(json!=null) {
-		routeNames=json.names;
-		notify(routeNames.length+' routes');
-	}
-	
 	// SAVE LOCATION
 	function saveLoc() {
 		var location=map.getCenter();
@@ -235,7 +222,6 @@
 		window.localStorage.setItem('lng',lng);
 		console.log('location saved: '+lng+','+lat );
 	}
-	
 	// TAP MAP
 	function mapTap(e) {
 		console.log('tap on map at '+e.latlng);
@@ -277,84 +263,18 @@
 		}
 		
 	}
-	
-	/* LIST TRACKS
-	function listTracks() {
-	    // show('menu',false);
-		notify('list '+trackNames.length+' tracks');
-		if(trackNames.length<1) return;
-		id('list').innerHTML="<li class='listItem'><b>TRACKS</b></li>";
-		for(var i=0; i<trackNames.length; i++) {
-		    // notify('track '+i);
-  			var listItem = document.createElement('li');
-  			listItem.classList.add('listItem');
-			var itemName = document.createElement('span');
-			itemName.index=i;
-			itemName.classList.add('itemName');
-			itemName.innerHTML=trackNames[i];
-			// notify('name: '+trackNames[i]);
-			itemName.addEventListener('click', function(){listIndex=this.index; loadTrack();});
-			var delButton = document.createElement('button');
-			delButton.index=i;
-			delButton.classList.add('deleteButton');
-			delButton.addEventListener('click', function() {listIndex=this.index; deleteTrack();});
-			// notify('delete button added for track '+i);
-			listItem.appendChild(itemName);
-			listItem.appendChild(delButton);
-			// trackList.appendChild(listItem);
-			id('list').appendChild(listItem);
-  		}
-  		// id('list').innerHTML+='<li onclick="clear()">CLEAR</li>';
-  		show('listScreen',true);
-		notify('track list populated with '+trackNames.length+' tracks');
-	}
-	*/
-	// LIST ROUTES
-	function listRoutes() {
-		console.log('list '+routeNames.length+' routes');
-		if(routeNames.length<1) return;
-		id('list').innerHTML='';
-		for(var i=0; i<routeNames.length; i++) {
-  			var listItem = document.createElement('li');
-  			// NEW CODE...
-  			listItem.index=i;
-  			listItem.innerText=routeNames[i];
-  			listItem.addEventListener('click',function(){listIndex=this.index; showRouteDetail();}); // select a route
-  			/* OLD CODE
-  			listItem.classList.add('listItem');
-			var itemName = document.createElement('span');
-			itemName.index=i;
-			itemName.classList.add('itemName');
-			itemName.innerHTML = routeNames[i];
-			itemName.addEventListener('click', function(){listIndex=this.index; loadRoute();});
-			var delButton = document.createElement('button');
-			delButton.index=i;
-			delButton.classList.add('deleteButton');
-			delButton.addEventListener('click', function() {listIndex=this.index; deleteRoute();});
-			listItem.appendChild(itemName);
-			listItem.appendChild(delButton);
-			*/
-			id('list').appendChild(listItem);
-  		}
-  		// id('list').innerHTML+='<li onclick="clear()">CLEAR</li>';
-		show('listScreen',true);
-		show('controls',false);
-		show('moreControls',false);
-	}
-	
 	// ADD TRACKPOINT
 	function addTP() {
 		var tp={};
 		tp.latlng=loc.latlng;
-		tp.alt=loc.alt;
-		notify('trackpoint '+trackpoints.length+' at '+tp.latlng+' alt: '+tp.alt);
+		// tp.alt=loc.alt;
+		notify('trackpoint '+trackpoints.length+' at '+tp.latlng);
 		tp.time=loc.time;
 		trackpoints.push(tp);
 		if(trackpoints.length<2) return;
 		if(trackpoints.length==2) track=L.polyline([trackpoints[0].latlng,trackpoints[1].latlng],{color:'black',weight:9,opacity:0.25}).addTo(map);
 		else if(trackpoints.length>2) track.addLatLng(tp.latlng);
 	}
-	
 	// LOCATION FIX
 	function getFix() { // get fix on current location
 	console.log('get a fix');
@@ -365,22 +285,6 @@
 		ready=true;
 		window.setTimeout(timeUp,15000); // revert to fix button after 15 secs
 	}
-	/*
-	function gotoFix(position) {
-		console.log("go to Fix");
-		loc.lng=position.latlng.longitude;
-		loc.lat=position.latlng.latitude;
-		loc.alt=position.latlng.altitude;
-		if(loc.alt!=null) loc.alt=Math.round(loc.alt);
-		notify("fix at "+loc.lng+","+loc.lat+","+loc.alt);
-		centreMap();
-		id("actionButton").innerHTML='<img src="goButton24px.svg"/>';
-		id("actionButton").removeEventListener("click", getFix);
-		id("actionButton").addEventListener("click", go);
-		ready=true;
-		window.setTimeout(timeUp,15000); // revert to fix button after 15 secs
-	}
-	*/
 	function timeUp() {
 		if(tracking) return;
 		console.log("times up - back to fix button");
@@ -389,8 +293,7 @@
 		id("actionButton").addEventListener("click", getFix);
 		ready=false;
 	}
-	
-	// START TRACKING
+	// TRACKING FUNCTIONS
 	function go() { // start tracking location
 		ready=false;
 		tracking=true;
@@ -413,13 +316,11 @@
 		id("actionButton").addEventListener("click", stopStart);
 		// show('measure',false);
 	}
-	
 	function stopStart() {
 		notify("stopStart");
 		if(tracking) pause();
 		else resume();
 	}
-	
 	function pause() { // pause location tracking
 		notify("PAUSE");
 		addTP(); // add trackpoint on pause
@@ -428,7 +329,6 @@
 		show('stopButton',true);
 		id("actionButton").innerHTML='<img src="goButton24px.svg"/>';
 	}
-	
 	function resume() { // restart tracking after pausing
 		notify("RESUME");
 		show('stopButton',false);
@@ -436,7 +336,6 @@
 		tracking = true;
 		map.locate({watch:true});
 	}
-	
 	function locationError(error) {
 		var message="";
 		switch (error.code) {
@@ -454,7 +353,6 @@
 		}
 		alert(message);
 	}
-	
 	// STOP TRACKING/ROUTING
 	function cease(event) {
 		/*
@@ -496,7 +394,6 @@
 		show('stopButton',false);
 		// show('routeLength',false);
 	}
-	
 	// POSITION MAP
 	function centreMap() { // move map to current location
 		// notify("centre map at N"+loc.lat+" E"+loc.lng);
@@ -505,73 +402,8 @@
 		var i, x, y;
 		saveLoc();
 	}
-
-	// SHOW TRACK PROFILES
-	function profiles() {
-		// notify('show track profiles?');
-		// show('menu',false);
-		if(trackpoints.length<5) return;
-		var n=trackpoints.length;
-		// draw altitude and speed profiles
-		var w=sw;
-		var h=sh*0.2; // was 0.4;
-		var minAlt=1000;
-		var maxAlt=0;
-		var maxSpeed=0;
-		// var avSpeed=distance*3.6/moving; // kph
-		notify('distance:'+distance+' average speed:'+avSpeed+'kph');
-		notify(n+" trackpoints");
-		// first create dark background
-		profilesCanvas.clearRect(0,0,w,h);
-		var gradient=profilesCanvas.createLinearGradient(0,0,0,h);
-		gradient.addColorStop(0,'#00000000');
-		gradient.addColorStop(1,'black');
-		profilesCanvas.fillStyle=gradient;
-		profilesCanvas.fillRect(0,0,w,h);
-		// speeds
-		var t=0; // time (sec)
-		var s=0; // compute maximium speed (km/hr)
-		for (i=1;i<n;i++) { // for each trackpoint
-			d=measure('distance',trackpoints[i-1].lng,trackpoints[i-1].lat,trackpoints[i].lng,trackpoints[i].lat);
-			// x+=d;
-			t=trackpoints[i].time-trackpoints[i-1].time;
-			s=3.6*d/t; // km/hr
-			if(s>maxSpeed) maxSpeed=s;
-		}
-		// elevation profile
-		profilesCanvas.beginPath();
-		profilesCanvas.lineWidth=3;
-	    profilesCanvas.strokeStyle = 'yellow'; // elevation profile is yellow
-		notify('ready to draw elevation profile');
-		var d=0;
-		var x,y;
-		for (var i=0;i<n;i++) {
-			t=trackpoints[i];
-			if(t.alt<minAlt) minAlt=t.alt;
-			if(t.alt>maxAlt) maxAlt=t.alt;
-			if(i>0) d+=measure('distance',t.lng,t.lat,trackpoints[i-1].lng,trackpoints[i-1].lat);
-			// notify('i:'+i+' d:'+d);
-			x=w*d/distance;
-			y=0.8*h-h*t.alt*20/distance;
-			// y=h*(maxAlt-t.alt)/dAlt;
-			if(i<1) profilesCanvas.moveTo(x,y);
-			else profilesCanvas.lineTo(x,y);
-			// notify('line to '+x+','+y);
-		}
-		profilesCanvas.stroke();
-		// elevations and speeds
-		profilesCanvas.font='16px Sans-Serif';
-		profilesCanvas.textBaseline='alphabetic';
-		profilesCanvas.fillStyle='white';
-		// maxSpeed=Math.round((metric)?maxSpeed:maxSpeed*0.62137);
-		// avSpeed=Math.round((metric)?avSpeed:avSpeed*0.62137);
-		profilesCanvas.fillText(minAlt+'-'+maxAlt+'m',10,h-5);
-		// notify("show profiles");
-		show('profilesPanel',true);
-	}
-	
 	// SAVE TRACK/ROUTE
-	function saver() {
+	function saveRoute() {
 		var name=id("saveName").value;
 		notify("save track/route as "+name);
 		if((routeNames.indexOf(name)>=0)) {
@@ -585,7 +417,6 @@
 			route.nodes=[];
 			for(var i=0;i<trackpoints.length();i++) {
 				route.nodes[i].latlng=trackpoints[i].latlng;
-				// route.nodes[i].alt=trackpoints[i].alt; // forget trackpoint.time
 			}
 		}
 		json=JSON.stringify(route);
@@ -599,49 +430,37 @@
 		distance=0;
 		show('saveDialog',false);
 	}
-	
-	/* LOAD TRACK
-	function loadTrack() {
-		notify('load track '+listIndex+": "+trackNames[listIndex]);
-		var json=window.localStorage.getItem(trackNames[listIndex]);
-		var track=JSON.parse(json);
-		distance=parseInt(track.distance);
-		duration=parseInt(track.duration);
-		moving=parseInt(track.moving);
-		trackpoints=track.trackpoints;
-		dist=0;
-		notify("load track with "+trackpoints.length+" trackpoints; length: "+distance+"m; duration: "+duration+"sec");
-		show('listScreen',false);
-		loc.latlng=trackpoints[0].latlng; // NEW
-		centreMap();
-		// REPLACE WITH track POLYLINE redraw();
-		show('actionButton',false);
-		profiles();
+	// LIST ROUTES
+	function listRoutes() {
+		console.log('list '+routeNames.length+' routes');
+		if(routeNames.length<1) return;
+		id('list').innerHTML='';
+		for(var i=0; i<routeNames.length; i++) {
+  			var listItem = document.createElement('li');
+  			// NEW CODE...
+  			listItem.index=i;
+  			listItem.innerText=routeNames[i];
+  			listItem.addEventListener('click',function(){listIndex=this.index; showRouteDetail();}); // select a route
+  			/* OLD CODE
+  			listItem.classList.add('listItem');
+			var itemName = document.createElement('span');
+			itemName.index=i;
+			itemName.classList.add('itemName');
+			itemName.innerHTML = routeNames[i];
+			itemName.addEventListener('click', function(){listIndex=this.index; loadRoute();});
+			var delButton = document.createElement('button');
+			delButton.index=i;
+			delButton.classList.add('deleteButton');
+			delButton.addEventListener('click', function() {listIndex=this.index; deleteRoute();});
+			listItem.appendChild(itemName);
+			listItem.appendChild(delButton);
+			*/
+			id('list').appendChild(listItem);
+  		}
+		show('listScreen',true);
+		show('controls',false);
+		show('more',false);
 	}
-	*/
-	// CLEAR TRACK/ROUTE
-	function clear() {
-	    notify('clear track/route');
-	    trackpoints=[];
-	    nodes=[];
-	    show('actionButton',true);
-	    // redraw();
-	}
-	
-	/* DELETE TRACK
-	function deleteTrack() {
-		var name=trackNames[listIndex];
-		alert('delete track '+listIndex+": "+name);
-		trackNames.splice(listIndex,1);
-		var tracks={};
-		tracks.names=trackNames;
-		var json=JSON.stringify(tracks);
-		window.localStorage.setItem("saxtonTracks",json);
-		window.localStorage.removeItem(name);
-		notify(name+" deleted");
-		show('listScreen',false);
-	}
-	*/
 	// SHOW ROUTE DETAIL
 	function showRouteDetail() {
 		notify('show detail for route '+listIndex+' - '+routeNames[listIndex]);
@@ -653,6 +472,21 @@
 		id('routeName').value=routeNames[listIndex];
 		id('routeLength').innerText=distance+unit;
 		show('routeDetail',true);
+	}
+	// RENAME ROUTE
+	function renameRoute() {
+		var oldName=routeNames[listIndex];
+		var newName=id('routeName').value;
+		notify('rename route '+listIndex+": "+oldName+ ' to '+newName);
+		routeNames[listIndex]=newName;
+		var json=window.localStorage.getItem(oldName);
+		window.localStorage.setItem(newName,json);
+		window.localStorage.removeItem(oldName);
+		var routes={};
+		routes.names=routeNames;
+		notify("save routenames: "+routes.names);
+		json=JSON.stringify(routes);
+		window.localStorage.setItem("saxtonRoutes",json);
 	}
 	// LOAD ROUTE
 	function loadRoute() {
@@ -673,11 +507,12 @@
 		console.log(points.length+' points: '+points);
 		track=L.polyline(points,{color:'green',weight:9,opacity:0.25}).addTo(map);
 	}
-	
 	// DELETE ROUTE
 	function deleteRoute() {
 		var name=routeNames[listIndex];
-		alert('delete route '+listIndex+": "+name);
+		var del=confirm('delete route '+name);
+		console.log('del is '+del);
+		if(del===false) return;
 		routeNames.splice(listIndex,1);
 		var routes={};
 		routes.names=routeNames;
@@ -687,9 +522,7 @@
 		notify(name+" deleted");
 		show('listScreen',false);
 	}
-
     // UTILITY FUNCTIONS
-	
 	function dm(degrees, lat) {
 	    var ddmm;
 	    var negative = false;
@@ -713,42 +546,18 @@
 	    ddmm+=decimal(n);
 	    return ddmm;
 	}
-	
-	/*
-	function measure(type,x0,y0,x,y) {
-		var dx = x - x0;
-	    var dy = y - y0;
-        dx *= 66610; // allow for latitude
-        dy *= 111111.111; // 90 deg = 10000 km
-		if(type=="distance") return Math.sqrt(dx * dx + dy * dy);
-		var h; // heading
-		if (dy == 0) {
-	        h = (dx > 0) ? 90 : 270;
-	    }
-	    else {
-	        h = Math.atan(dx / dy) * 180 / Math.PI;
-	        if (dy < 0) h += 180;
-	        if (h < 0) h += 360 // range 0-360
-        }
-        return h;
-	}
-	*/
-	
 	function decimal(n) {
 	    return Math.floor(n * 10 + 0.5) / 10;
 	}
-	
 	function show(element,visible) {
 //		console.log('show '+element+' - '+visible);
 	    id(element).style.display=(visible)?'block':'none';
 	}
-	
 	function notify(note) {
 		notifications.push(note);
 		while(notifications.length>30) notifications.shift();
 		console.log(note);
 	}
-	
 	function showNotifications() {
 		var message="";
 		for(var i in notifications) {
@@ -756,7 +565,6 @@
 		}
 		alert(message);
 	}
-	
 	function id(el) {
 	    return document.getElementById(el);
 	}
